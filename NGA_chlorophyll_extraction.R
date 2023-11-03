207b41b847084fc68554814b7561de6d64a0159d
njlyon0 <nicholasjlyon@gmail.com>
2023-11-03 08:43:36 -0400

    Implemented non-raster fix for identifying chlorophyll values in shapefile

--- NGA_chlorophyll_extraction.R
+++ NGA_chlorophyll_extraction.R
@@ -60,12 +60,48 @@ plot(GAK_shapefile, axes = T)
 
 # Read in chlorophyll csv
 chloro <- read.csv(file = file.path("raw_data", site, "Chlo_GAK.csv")) %>%
+  # Add an ID column
+  dplyr::mutate(id = 1:nrow(.), .before = dplyr::everything()) %>% 
   # Move the time column
-  dplyr::relocate(time, .after = chlorophyll)
+  dplyr::relocate(time, .after = chlorophyll) %>%
+  # Remove missing chlorophyll values
+  ## Makes a smaller/more manageable data object going forward
+  dplyr::filter(!is.na(chlorophyll))
 
-# Convert chlorophyll csv to raster
-chloro_rast <- terra::rast(chloro, type="xyz", crs="4326")
-# Error: [raster,matrix(xyz)] x cell sizes are not regular
+# Make a spatial variant
+chloro_sf <- chloro %>% 
+  sf::st_as_sf(coords = c("longitude", "latitude"), 
+               crs = sf::st_crs(GAK_shapefile))
 
+# Check whether that worked
+plot(GAK_shapefile, axes = T, reset = F)
+plot(chloro_sf, add = T, col = "black", pch = 20)
 
+# Identify whether each point intersects the shapefile
+chloro_extract <- chloro_sf %>%
+  # Get a "1" if the point is in the shapefile or "NA" if not
+  dplyr::mutate(ixn = as.integer(sf::st_intersects(x = geometry, y = GAK_shapefile))) %>%
+  # Filter to only points that do interesect the shapefile
+  dplyr::filter(!is.na(ixn))
 
+# Check structure of that
+dplyr::glimpse(chloro_extract)
+
+# Should have far fewer rows than we started with
+nrow(chloro); nrow(chloro_extract)
+
+# Make another test graph
+plot(GAK_shapefile, axes = T, reset = F)
+plot(chloro_extract, add = T, col = "black", pch = 20)
+
+# Do some final object processing
+chloro_actual <- chloro_extract %>%
+  # Drop sf geometry
+  sf::st_drop_geometry(x = .) %>% 
+  # Re-attach coordinates to the data
+  dplyr::left_join(y = chloro, by = c("id", "chlorophyll", "time")) %>% 
+  # Re-order columns (implicitly drop ixn and ID column)
+  dplyr::select(latitude, longitude, chlorophyll, time)
+
+# Final structure check
+dplyr::glimpse(chloro_actual)
