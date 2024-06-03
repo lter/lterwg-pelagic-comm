b06b4c38c133d52c2fb0b596cecfb7e9f3bb5b3c
Alexandra Cabanelas <acabanelas@whoi.edu>
2024-06-03 10:16:44 -0400

    cleaned up code, added comments

--- R/doubleIntegrationAndCorr_customTAU.R
+++ R/doubleIntegrationAndCorr_customTAU.R
@@ -2,30 +2,42 @@
 #############          Pelagic Synthesis           #############################
 #############             MAR-2024                 #############################
 #############          Double Integration          #############################
-## by: Alexandra Cabanelas #####################################################
+## by: Alexandra Cabanelas 
 ################################################################################
-# Double Integration - setting custom TAU for different taxa
+## Double Integration Analysis NES - ECOMON
+# Script #1 : doubleIntegrationAndCorr_customTAU
 
-## Packages
-library(tidyverse)
-library(fasttime) #because regular posixt function takes too long in loop
-library(gridExtra) 
-library(cowplot)
+# script to calculate bio anomalies, perform integrations, and plot
+# code allows to specify custom TAU (life span) for diff taxa 
+
+## ------------------------------------------ ##
+#            Packages -----
+## ------------------------------------------ ##
+
+library(tidyverse)#v2.0.0
+#library(fasttime) #because regular posixt function takes too long in loop; v1.1.0
+library(gridExtra) #v2.3 
+library(cowplot) #v1.1.3
 
 #install.packages("devtools")
 #remotes::install_github("noaa-edab/ecodata",build_vignettes=TRUE) 
 #or pak::pkg_install("noaa-edab/ecodata")
-library(ecodata) #coldpool and 
+library(ecodata) #coldpool and slopewater
 #https://github.com/NOAA-EDAB/ecodata
 
-library(listviewer) #for looking at lists interactively 
-library(magrittr) #map_dfr
+library(listviewer) #v4.0.0; for looking at lists interactively 
+library(magrittr) #v2.0.3; map_dfr
+library(here) #v1.0.1; easily build path to files  
+
 #install.packages("librarian") #to get files from googledrive
 #library(librarian) #not using this route at the moment
 #install.packages(c("googledrive", "httpuv"))
 library(googledrive) #for accessing data files in google drive folder
 ################################################################################
-## Data
+
+## ------------------------------------------ ##
+#            Data -----
+## ------------------------------------------ ##
 
 #not sure if i need the dir.create lines..?
 #dir.create(path = file.path("raw_data"), showWarnings = F)
@@ -41,11 +53,26 @@ drive_folder <- googledrive::drive_ls(path = "https://drive.google.com/drive/u/0
 
 abu <- googledrive::drive_download(file = drive_folder$id) #im stuck here.. how to assign this file to "abu"?
 
-abu <- read.csv("raw/EcoMon_v3_8_wDateStrata.csv")
+# can download EcoMon data from 
+#https://www.ncei.noaa.gov/access/metadata/landing-page/bin/iso?id=gov.noaa.nodc:0187513 
+# NCEI Accession 0187513 v3.3 
 
-taxa_of_interest <- c("name1", "name2", "name3")
+# preconditions: raw EcoMon data was processed to georeference based on the two 
+# provided shapefiles: EcomonStrata_v4.shp & EcomonStrata_v4b.shp
+# to classify the distinct ecoregions within the NES using the provided 108 
+# spatial polygons (e.g., identify Gulf of Maine, Georges Bank, 
+# Southern New England, and Mid-Atlantic Bight 
+
+abu <- read.csv(file.path("raw","EcoMon_v3_8_wDateStrata.csv"))
 
 ################################################################################
+# using 10m2 values; 100m3 data is available
+names(abu)<-gsub("_10m2","",names(abu)) # get rid of _10m2 in colnames
+
+# Select taxa to analyze
+#change if want more/different taxa
+taxa_of_interest <- c("ctyp", "calfin", "pseudo")
+
 # add season to df
 abu <- abu %>%
   mutate(season = case_when(between(month, 3, 5) ~ "spring",
@@ -54,37 +81,33 @@ abu <- abu %>%
                             TRUE ~ "winter"))
 # add region to df
 abu <- abu %>%
-  mutate(Region = case_when(region == 1 ~ "MAB",
-                            region == 2 ~ "SNE",
-                            region == 3 ~ "GB",
-                            region == 4 ~ "GOM",
+  mutate(Region = case_when(region == 1 ~ "MAB", #MidAtlantic Bight
+                            region == 2 ~ "SNE", #Southern New England
+                            region == 3 ~ "GB", #Georges Bank
+                            region == 4 ~ "GOM", #Gulf of Maine
                             TRUE ~ "Outside"))
-################################################################################
-#select cols of interest - change if wanna do more/different taxa
 
-abu1 <- abu[ , c(11:14,23:25, 75, 79, 208, 235, 298:299)] #this selects ctypicus and cfinmarchicus
+# select cols of interest
 abu1 <- abu %>%
-  select(year, month, taxa_of_interest)
-#throwing in pseudocalanus, megan, euphkr, clupea harengus, ammodytes
+  select(date, month, day, year, all_of(taxa_of_interest), season, Region)
 
-# Looking just at 10m2 for now, but can change by selecting
-# the other columns with the 100m3 values 
-
-names(abu1)<-gsub("_10m2","",names(abu1)) #to get rid of _10m2 in colnames
-
-#change data from wide to long
+# pivot from wide to long
 abu_long <- abu1 %>%
   pivot_longer(cols = c(ctyp:ammspp),
                names_to = "taxa", values_to = "abundance")
 
 #remove rows with NANS = no sampling for zp or itchyo 
+#zoo_not_ich = 3437
+#ich_not_zoo = 3627
+#both = 25629      
 #both gears not always used
 abu_long <- abu_long %>% filter(!is.nan(abundance))
+
 ################################################################################
 # Define a list of taxa and regions - for the loop below
 # not essential; used it for regions to exclude "outside" 
 # can use it to select specific taxa; but we also filtered those out above in 
-#line 34
+#line 74
 
 #taxa_list <- c("ctyp", "calfin")  
 region_list <- c("MAB", "SNE", "GOM", "GB")  #excluding "outside"
@@ -110,7 +133,9 @@ region_list <- c("MAB", "SNE", "GOM", "GB")  #excluding "outside"
 #     b. Spatial average
 #     c. Z-score transformation for plotting on same axes
 
-######### Loop to calculate anomalies
+## ------------------------------------------ ##
+#     Calculate anomalies/biological TS -----
+## ------------------------------------------ ##
 
 zscore_list <- list()
 
@@ -160,7 +185,8 @@ for (taxa in unique(abu_long$taxa)) {
       
       # Merge with abu_longTran to include the date column
       zscore2 <- zscore1 %>%
-        left_join(abu_longTran %>% select(season, year, date), by = c("season", "year"))
+        left_join(abu_longTran %>% select(season, year, date), 
+                  by = c("season", "year"))
       
       # Convert date column to POSIXct
       #zscore2$date <- fastPOSIXct(zscore2$date) #this attaches time to date
@@ -181,12 +207,17 @@ for (taxa in unique(abu_long$taxa)) {
     }
   }
 }
+
 zscore <- do.call(rbind, zscore_list) 
-#write.csv(zscore, "output/zscore_anomalies_moretaxa.csv")
-rm(zscore1, zscore2)
+rm(zscore1, zscore2, abu_longMean, abu_longSD, abu_longTran)
 
-zscore_list <- zscore_list[-which(names(zscore_list) == "ammspp_SNE_fall")]
+ggplot(zscore, aes(x=year, y=Anomaly_yr)) + 
+  geom_line() + 
+  facet_grid(taxa ~ season+Region)
 
+#zscore_list <- zscore_list[-which(names(zscore_list) == "ammspp_SNE_fall")]
+
+# check SD and mean of anomalies by group
 zscore %>%
   group_by(taxa, Region, season) %>%
   summarize(mean_Anomaly_yr = mean(Anomaly_yr, na.rm = TRUE),
@@ -236,11 +267,11 @@ calculateIntegrations = function(data, tau, f = function(x) {mean(x, na.rm = T)}
 tau_values <- c(
   "ctyp" = 30,
   "calfin" = 365*1,
-  "pseudo" = 90,
-  "megan" = 365*2,
-  "euphkr" = 365*1,
-  "cluhar" = 365*5,
-  "ammspp" = 365*4
+  "pseudo" = 90
+  #"megan" = 365*2,
+  #"euphkr" = 365*1,
+  #"cluhar" = 365*5,
+  #"ammspp" = 365*4
 )
 
 
@@ -260,65 +291,43 @@ amo <- read.csv("raw/amo_readyDI.csv", header = TRUE)
 
 # Convert character to POSIXct
 amo$time <- as.POSIXct(amo$time, format = "%d-%m-%y")
-#amo$time <- fastPOSIXct(amo$time) #this attaches time to date
-#amo$time <- as.Date(amo$time)
 
-#amoInt <- calculateIntegrations(amo, tau_values)
-#amoInt$time <- as.Date(amoInt$time)
-#write.csv(amoInt, "amoIntegrations.csv")
-#driver_amo <- amoInt[, c(1,3)]
-#int1_amo <- amoInt[, c(1,4)]
-#int2_amo <- amoInt[, c(1,5)]
 #################
 
 ##                            NAO - North Atlantic Oscillation                ##
 #https://www.ncei.noaa.gov/access/monitoring/nao/
 #https://www.cpc.ncep.noaa.gov/products/precip/CWlink/pna/norm.nao.monthly.b5001.current.ascii
 # 1973-2023
-nao_month_long <- read.csv("raw/norm.nao.monthly.b5001.current.ascii.LONG.csv")
-nao_month_long$time <- as.Date(paste(nao_month_long$yr, nao_month_long$month, "15", sep = "-"))
-nao_month_long <- nao_month_long %>% filter(yr > 1972)
-nao_month_long <- nao_month_long[, c("time", "nao", "yr", "month")]
-nao_month_long1 <- nao_month_long[, c(1:2)]
-nao_month_long1$time <- as.POSIXct(nao_month_long1$time, format = "%Y-%m-%d")
-nao_month_long1 <- nao_month_long1[-611 ,]
+
+#nao_month_long <- read.csv("raw/norm.nao.monthly.b5001.current.ascii.LONG.csv")
+#nao_month_long$time <- as.Date(paste(nao_month_long$yr, nao_month_long$month, "15", sep = "-"))
+#nao_month_long <- nao_month_long %>% filter(yr > 1972)
+#nao_month_long <- nao_month_long[, c("time", "nao", "yr", "month")]
+#nao_month_long1 <- nao_month_long[, c(1:2)]
+#nao_month_long1$time <- as.POSIXct(nao_month_long1$time, format = "%Y-%m-%d")
+#nao_month_long1 <- nao_month_long1[-611 ,]
 
 nao_month_long <- read.csv(file.path("raw", "norm.nao.monthly.b5001.current.ascii.LONG.csv")) %>%
   mutate(time = as.Date(paste(yr, month, "15", sep = "-"))) %>%
-  filter(yr > 1972) %>%
-  select(time, nao, yr, month) %>%
-  select(1,2) %>%
-  mutate(time = as.POSIXct(time, format = "%Y-%m-%d")) %>%
-  filter(time =! as.POSIXct("2023-11-15"))
+  filter(yr > 1972 & 
+           time < as.Date("2023-11-15")) %>%
+  select(time, nao) %>%
+  mutate(time = as.POSIXct(time, format = "%Y-%m-%d")) 
 
-
-#naoInt <- calculateIntegrations(nao_month_long1)
-#naoInt$time <- as.Date(naoInt$time)
-#write.csv(naoInt, "output/naoIntegrations.csv")
-#driver_nao <- naoInt[, c(1,3)]
-#int1_nao <- naoInt[, c(1,4)]
-#int2_nao <- naoInt[, c(1,5)]
 #################
 
 ##                           AO - Arctic Oscillation                          ##
 # https://www.cpc.ncep.noaa.gov/products/precip/CWlink/daily_ao_index/monthly.ao.index.b50.current.ascii
 # https://www.cpc.ncep.noaa.gov/products/precip/CWlink/daily_ao_index/ao.shtml
 # 1973-2023
-ao <- read.csv("raw/ao.csv")
-ao$date <- as.Date(paste(ao$year, ao$month, "15", sep = "-"))
-ao <- ao %>% filter(year > 1972)
-ao <- ao[, -c(1:2)]
-ao$date <- as.POSIXct(ao$date, format = "%Y-%m-%d")
-ao <- ao[, c("date", "ao")]
-colnames(ao)[1] <- 'time'
-ao <- ao[-611 ,]
 
-#aoInt <- calculateIntegrations(ao)
-#aoInt$time <- as.Date(aoInt$time)
-#write.csv(aoInt, "output/aoIntegrations.csv")
-#driver_ao <- aoInt[, c(1,3)]
-#int1_ao <- aoInt[, c(1,4)]
-#int2_ao <- aoInt[, c(1,5)]
+ao <- read.csv(file.path("raw", "ao.csv")) %>%
+  mutate(time = as.Date(paste(year, month, "15", sep = "-"))) %>% # 
+  filter(year > 1972 & 
+           time < as.Date("2023-11-15")) %>%
+  select(time, ao) %>%
+  mutate(time = as.POSIXct(time, format = "%Y-%m-%d"))
+
 #################
 
 ##                               GSI - Gulf Stream Index                      ##
@@ -327,25 +336,17 @@ ao <- ao[-611 ,]
 #write.csv(gsi, "raw/gsi.csv") downloaded 26-MAR-2024
 # 1954-2023
 
-gsi <- read.csv("raw/gsi.csv")
-gsi <- gsi %>% filter(Var == "gulf stream index") #there is also western gsi ..? 
-gsi$Date <- as.Date(sprintf("%.2f.01", gsi$Time), format = "%Y.%m.%d")
-gsi <- gsi[, -c(1, 3, 5)]
-gsi$Time <- floor(gsi$Time)
-gsi <- gsi %>% filter(Time > 1972)
-gsi <- gsi[, -1]
-gsi$Date <- as.POSIXct(gsi$Date, format = "%Y-%m-%d")
-gsi <- gsi[, c("Date", "Value")]
-colnames(gsi)[1] <- 'time'
-colnames(gsi)[2] <- 'gsi'
+gsi <- read.csv(file.path("raw", "gsi.csv")) %>%
+  filter(Var == "gulf stream index") %>% #there is also western gsi ..?
+  mutate(Date = as.Date(sprintf("%.2f.01", Time), format = "%Y.%m.%d")) %>%
+  select(Time, Value, Date) %>% 
+  mutate(Time = floor(Time)) %>%
+  filter(Time > 1972) %>%
+  select(Date, Value) %>%
+  mutate(Date = sub("\\d{2}$", "15", Date)) %>%  # change day to 15th
+  mutate(Date = as.POSIXct(Date, format = "%Y-%m-%d")) %>% #class POSIXct
+  rename(time = Date, gsi = Value)
 
-#gsiInt = calculateIntegrations(gsi)
-#gsiInt$time <- as.Date(gsiInt$time)
-
-#write.csv(gsiInt, "output/gsiIntegrations.csv")
-#driver_gsi <- gsiInt[, c(1,3)]
-#int1_gsi <- gsiInt[, c(1,4)]
-#int2_gsi <- gsiInt[, c(1,5)]
 
 # Create a list of data frames, one for each tau value
 amoInt_list <- lapply(tau_values, function(tau) calculateIntegrations(amo, tau))
@@ -353,59 +354,12 @@ naoInt_list <- lapply(tau_values, function(tau) calculateIntegrations(nao_month_
 aoInt_list <- lapply(tau_values, function(tau) calculateIntegrations(ao, tau))
 gsiInt_list <- lapply(tau_values, function(tau) calculateIntegrations(gsi, tau))
 
-#have to figure out a way to save these for next scripts...
-
 listviewer::jsonedit(amoInt_list)
+listviewer::jsonedit(amoInt_list)
+listviewer::jsonedit(zscore_list)
 
-
-#par(mfrow = c(3,1))
-#for (n in names(amo)[-1]) {
-#  for (tau_name in names(amoInt_list)) {
-#    plot(amo[,1], amoInt_list[[tau_name]][[n]], type = 'l', ylab = n, xlab = 'Date') # Plot each integration
-#    mtext(paste("TAU:", tau_values[tau_name]), side = 3, line = -2, adj = 1) # Add TAU value annotation
-#    plot(amo[,1], amoInt_list[[tau_name]][[paste0(n,'Int')]], type = 'l', ylab = paste0(n, ' Int'), xlab = 'Date') # Plot Int
-    #mtext(paste("TAU:", tau_values[tau_name]), side = 3, line = -2, adj = 1) # Add TAU value annotation
-#    plot(amo[,1], amoInt_list[[tau_name]][[paste0(n,'DInt')]], type = 'l', ylab = paste0(n, ' DInt'), xlab = 'Date') # Plot DInt
-    #mtext(paste("TAU:", tau_values[tau_name]), side = 3, line = -2, adj = 1) # Add TAU value annotation
-#  }
-#}
-
-#par(mfrow = c(3,1))
-#for (n in names(nao_month_long1)[-1]) {
-#  for (tau_name in names(naoInt_list)) {
-#    plot(nao_month_long1[,1], naoInt_list[[tau_name]][[n]], type = 'l', ylab = n, xlab = 'Date') # Plot each integration
-#    mtext(paste("TAU:", tau_values[tau_name]), side = 3, line = -2, adj = 1) # Add TAU value annotation
-#    plot(nao_month_long1[,1], naoInt_list[[tau_name]][[paste0(n,'Int')]], type = 'l', ylab = paste0(n, ' Int'), xlab = 'Date') # Plot Int
-#    #mtext(paste("TAU:", tau_values[tau_name]), side = 3, line = -2, adj = 1) # Add TAU value annotation
-#    plot(nao_month_long1[,1], naoInt_list[[tau_name]][[paste0(n,'DInt')]], type = 'l', ylab = paste0(n, ' DInt'), xlab = 'Date') # Plot DInt
-#    #mtext(paste("TAU:", tau_values[tau_name]), side = 3, line = -2, adj = 1) # Add TAU value annotation
-#  }
-#}
-
-#par(mfrow = c(3,1))
-#for (n in names(ao)[-1]) {
-#  for (tau_name in names(aoInt_list)) {
-#    plot(ao[,1], aoInt_list[[tau_name]][[n]], type = 'l', ylab = n, xlab = 'Date') # Plot each integration
-#    mtext(paste("TAU:", tau_values[tau_name]), side = 3, line = -2, adj = 1) # Add TAU value annotation
-#    plot(ao[,1], aoInt_list[[tau_name]][[paste0(n,'Int')]], type = 'l', ylab = paste0(n, ' Int'), xlab = 'Date') # Plot Int
-#    #mtext(paste("TAU:", tau_values[tau_name]), side = 3, line = -2, adj = 1) # Add TAU value annotation
-#    plot(ao[,1], aoInt_list[[tau_name]][[paste0(n,'DInt')]], type = 'l', ylab = paste0(n, ' DInt'), xlab = 'Date') # Plot DInt
-#    #mtext(paste("TAU:", tau_values[tau_name]), side = 3, line = -2, adj = 1) # Add TAU value annotation
-#  }
-#}
-
-#par(mfrow = c(3,1))
-#for (n in names(gsi)[-1]) {
-#  for (tau_name in names(gsiInt_list)) {
-#    plot(gsi[,1], gsiInt_list[[tau_name]][[n]], type = 'l', ylab = n, xlab = 'Date') # Plot each integration
-#    mtext(paste("TAU:", tau_values[tau_name]), side = 3, line = -2, adj = 1) # Add TAU value annotation
-#    plot(gsi[,1], gsiInt_list[[tau_name]][[paste0(n,'Int')]], type = 'l', ylab = paste0(n, ' Int'), xlab = 'Date') # Plot Int
-#    #mtext(paste("TAU:", tau_values[tau_name]), side = 3, line = -2, adj = 1) # Add TAU value annotation
-#    plot(gsi[,1], gsiInt_list[[tau_name]][[paste0(n,'DInt')]], type = 'l', ylab = paste0(n, ' DInt'), xlab = 'Date') # Plot DInt
-#    #mtext(paste("TAU:", tau_values[tau_name]), side = 3, line = -2, adj = 1) # Add TAU value annotation
-#  }
-#}
-
+###############
+# plot TS of drivers with 3 double integration 
 
 plotData <- function(data, int_list, tau_values) {
   par(mfrow = c(3,1))
@@ -420,21 +374,20 @@ plotData <- function(data, int_list, tau_values) {
   }
 }
 
-# Now you can call this function for each of your datasets
+# plot each driver with varying tau values 
 plotData(amo, amoInt_list, tau_values)
 plotData(nao_month_long1, naoInt_list, tau_values)
 plotData(ao, aoInt_list, tau_values)
 plotData(gsi, gsiInt_list, tau_values)
 
-
-listviewer::jsonedit(amoInt_list)
-listviewer::jsonedit(zscore_list)
 ################################################################################
 ################################################################################
 # 3) Test correlations between bio TS + each driver TS 
 #                                   Correlations 
-# List to store the correlations for each combination
+
+# List to store the correlations and pvalues for each combination
 correlation_list <- list()
+p_value_list <- list()
 
 # Iterate over each group (taxa_region_season) in zscore_list
 for (group_name in names(zscore_list)) {
@@ -448,6 +401,7 @@ for (group_name in names(zscore_list)) {
   
   # List to store correlations for each combination of taxa and driver
   group_correlations <- list()
+  group_pvalues <- list()
   
   # Iterate over each atmospheric driver
   for (driver_name in c("amo", "nao", "ao", "gsi")) {  # Drivers
@@ -490,17 +444,18 @@ for (group_name in names(zscore_list)) {
         
         # Store correlations for the current combination of taxa, driver, and integrations
         group_correlations[[paste0(group_name, "_", driver_name, "_", taxa_name)]] <- correlations
+        group_pvalues[[paste0(group_name, "_", driver_name, "_", taxa_name)]] <- p_values
       }
     }
   }
   
-  # Store correlations for the current group
+  # Store correlations and pvals for the current group
   correlation_list[[group_name]] <- group_correlations
+  p_value_list[[group_name]] <- group_pvalues
 }
 
-
 correlation_list
-
+p_value_list
 ###############################################################################
 ################################################################################
 
